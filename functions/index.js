8a102cbd6b36aa3a22ae1f920932f8e7144dc4d9
multiarts <jmello@hotmail.com.br>
2020-03-09 20:22:31 -0300

    UPDATE

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,41 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// On sign up.
+exports.processSignUp = functions.auth.user().onCreate(async user => {
+  console.log(user);
+  // Check if user meets role criteria:
+  // Your custom logic here: to decide what roles and other `x-hasura-*` should the user get
+  let customClaims;
+  if (user.email && user.email.indexOf('@hasura.io') != -1) {
+    customClaims = {
+      'https://hasura.io/jwt/claims': {
+        'x-hasura-default-role': 'admin',
+        'x-hasura-allowed-roles': ['user', 'admin'],
+        'x-hasura-user-id': user.uid
+      }
+    };
+  }
+  else {
+    customClaims = {
+      'https://hasura.io/jwt/claims': {
+        'x-hasura-default-role': 'user',
+        'x-hasura-allowed-roles': ['user'],
+        'x-hasura-user-id': user.uid
+      }
+    };
+  }
+  // Set custom user claims on this newly created user.
+  try {
+        await admin.auth().setCustomUserClaims(user.uid, customClaims);
+        // Update real-time database to notify client to force refresh.
+        const metadataRef = admin.database().ref("metadata/" + user.uid);
+        // Set the refresh time to the current UTC timestamp.
+        // This will be captured on the client to force a token refresh.
+        return metadataRef.set({ refreshTime: new Date().getTime() });
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
--- lib/app/config/client.dart
+++ lib/app/config/client.dart
@@ -6,7 +6,6 @@ class Config {
   static final HttpLink httpLink =
       HttpLink(
         uri: "https://jhoneburger.herokuapp.com/v1/graphql",
-        // headers: {"HASURA_GRAPHQL_ADMIN_SECRET":"xerebenebias.123.isaac.julia"}
       );
 
   static final AuthLink authLink = AuthLink(getToken: () async => await sharedPreferenceService.token);
@@ -19,9 +18,7 @@ class Config {
     ),
   );
 
-  // static final Link link = authLink.concat(httpLink).concat(websocketLink);
   static final Link link = authLink.concat(httpLink).concat(websocketLink);
-  // static final Link link = httpLink;
   
   static ValueNotifier<GraphQLClient> initailizeClient() {
     ValueNotifier<GraphQLClient> client = ValueNotifier(
